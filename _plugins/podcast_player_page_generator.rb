9a6c54d6a0738f9a6ce8d947a976d693d79f1156
Stefan Haslinger <stefan.haslinger@mittenin.at>
2016-03-24 10:33:59 +0100

    debugging ...

--- _plugins/podcast_player_page_generator.rb
+++ _plugins/podcast_player_page_generator.rb
@@ -1,25 +1,19 @@
 module Jekyll
-  class PodcastPlayerDocument < Document
-     def initialize(path, post, relations = {})
-      @site = relations[:site]
-      @path = path
-      @extname = File.extname(path)
-      @collection = relations[:collection]
-      @has_yaml_header = true
-      categories_from_path(collection.relative_directory)
+  class PodcastPlayerPage < Page
+    def initialize(site, base, dir, post)
+      @site = site
+      @base = base
+      @dir  = dir
+      @name = post.basename.sub!('episode','player')
 
-      data.default_proc = proc do |_, key|
-        site.frontmatter_defaults.find(relative_path, collection.label, key)
-      end
+      self.process(@name)
+      self.read_yaml(File.join(base, '_layouts'), 'player.html')
 
-      self.data['layout'] = 'player'
       self.data['title'] = post.data['title']
       self.data['author'] = post.data['author']
       self.data['explicit'] = post.data['explicit']
       self.data['audio'] = post.data['audio']
       self.data['chapters'] = post.data['chapters']
-      self.content = '{{ page | web_player:site }}'
-      trigger_hooks(:post_init)
     end
   end
 
@@ -27,9 +21,14 @@ module Jekyll
     safe true
 
     def generate(site)
-      if site.layouts.key? 'player_template'
+      if site.layouts.key? 'player'
         site.posts.docs.each do |post|
-          site.collections['players'].docs << PodcastPlayerDocument.new(site.collections['players'].collection_dir, post, { collection: 'players', site: site })
+          dir =  site.config['players_dir'] || 'players'
+          site.posts.docs.each do |post|
+            player = PodcastPlayerPage.new(site, site.source, dir, post)
+            player.write('/')
+            site.pages << player
+          end
         end
       end
     end
